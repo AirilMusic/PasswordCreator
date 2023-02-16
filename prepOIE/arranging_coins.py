85fabf1212ed53fae0da826906fd1c59487ae9f0
Airil <60015796+AirilMusic@users.noreply.github.com>
2023-02-16 11:48:40 +0100

    Update arranging_coins.py

--- prepOIE/arranging_coins.py
+++ prepOIE/arranging_coins.py
@@ -3,17 +3,21 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         tr = 0
+        tr0 = 0
         t = 0
         n2 = n
         while n2 > 0:
             t += 1
             n2 -= t
+            tr0 += 1
         print(t)
-        
-        for i in range(t):
-            n -= i+1
-            if n > 0:
-                tr += 1
-            else:
-                break
-        return(tr)
+        if n  == (t * (t + 1))/2:
+            return(tr0)
+        else:        
+            for i in range(t):
+                n -= i+1
+                if n > 0:
+                    tr += 1
+                else:
+                    break
+            return(tr)
