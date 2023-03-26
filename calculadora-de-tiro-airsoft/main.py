f5cdbe66de6c3dc4a208b4b00fe632ec30c25034
Airil <60015796+AirilMusic@users.noreply.github.com>
2023-03-26 03:41:54 +0200

    Update main.py

--- calculadora-de-tiro-airsoft/main.py
+++ calculadora-de-tiro-airsoft/main.py
@@ -5,8 +5,6 @@ from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
 from kivy.uix.scrollview import ScrollView
 import calculator
-#print("vertical:", calculator.altura(100, 450, 0.5, 0.6))
-#print("horizontal:", calculator.viento(True, 100, 450, 0.5, 0.6, 5, 90, 10))
 
 class Ui(ScreenManager):
     def calculate_data(self):
@@ -15,9 +13,26 @@ class Ui(ScreenManager):
         barrel_length = float(self.ids.barrel_length.text)
         weight = float(self.ids.weight.text)
         diameter = float(self.ids.diameter.text)
+        wspeed = float(self.ids.wspeed.text)
+        wdirection = float(self.ids.wdirection.text)
+        obstacles = int(self.ids.obstacles.text)
         
-        print("Distancia:", distance, "Velocidad:", speed, " Longitud del cañón:", barrel_length, " Peso de la munición:", weight, " Diámetro de la munición:", diameter)
-
+        print("Distancia:", distance, "Velocidad:", speed, " Longitud del cañón:", barrel_length, " Peso de la munición:", weight, " Diámetro de la munición:", diameter, " Wind speed:", wspeed, " Wind direction:", wdirection, " Obstacles:", obstacles)
+        
+        t, height, desplazamiento = 0, 0, 0
+        t = calculator.time(distance, speed, weight, diameter)
+        height = calculator.altura(distance, speed, weight, diameter)
+        if obstacles != 0:
+            desplazamiento = calculator.viento(True, distance, speed, weight, diameter, obstacles, wdirection, wspeed)
+        else:
+            desplazamiento = calculator.viento(False, distance, speed, weight, diameter, obstacles, wdirection, wspeed)
+            
+        print("Tiempo:", t, "Altura:", height, "Desplazamiento:", desplazamiento)
+        
+        self.ids.time.text = str(t)
+        self.ids.height.text = str(height)
+        self.ids.desplazamiento.text = str(desplazamiento)
+        
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = 'Dark'
